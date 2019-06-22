8292831ba6ac830d6e03572a9e91edd44a69be90
Todd Chen <toddchen@nd.com.cn>
2019-06-22 19:57:12 +0800

    feat: apollo server setting

---
+++ start/server/src/index.js
@@ -0,0 +1,40 @@
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
+const { createStore } = require('./utils')
+const resolvers = require('./resolvers')
+const isEmail = require('isemail')
+
+const LaunchAPI = require('./datasources/launch')
+const UserAPI = require('./datasources/user')
+
+const store = createStore()
+
+// the function that sets up the global context for each resolver, using the req
+const context = async ({ req }) => {
+  // simple auth check on every request
+  const auth = (req.headers && req.headers.authorization) || '';
+  const email = new Buffer(auth, 'base64').toString('ascii');
+
+  // if the email isn't formatted validly, return null for user
+  if (!isEmail.validate(email)) return { user: null };
+  // find a user by their email
+  const users = await store.users.findOrCreate({ where: { email } });
+  const user = users && users[0] ? users[0] : null;
+
+  return { user: { ...user.dataValues } };
+};
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+})
+
+server.listen().then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`);
+})
---
+++ start/server/src/schema.js
@@ -0,0 +1,74 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+
+    missionPatch(mission: String, size: PatchSize): String
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
+
+  type LaunchConnection { # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+`
+
+module.exports = typeDefs
